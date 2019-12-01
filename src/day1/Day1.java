240f64fe0dd0357c9410195476e99fd5a59b46b6
Katie Sanderson <katherina.sanderson@gmail.com>
2019-12-01 14:40:05 -0600

    Day 1, Part 2 solution

--- src/day1/Day1.java
+++ src/day1/Day1.java
@@ -8,6 +8,15 @@ public class Day1 {
 
     public static void main(String[] args) throws IOException {
         System.out.println(Files.readAllLines(FileSystems.getDefault().getPath("src", "day1", "input.txt")).stream()
-                .map(Integer::parseInt).reduce(0, (sum, val) -> sum + (val / 3 - 2)));
+                .map(Integer::parseInt).reduce(0, (sum, val) -> sum + calculateRequiredGas(val)));
+    }
+
+    private static int calculateRequiredGas(int val) {
+        int sum = 0;
+        while (val > 0) {
+            val = val / 3 - 2;
+            sum += Math.max(val, 0);
+        }
+        return sum;
     }
 }
