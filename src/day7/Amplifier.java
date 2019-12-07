386d5f4767a72d52d3cc3d3743657bcfe4cc0a95
Katie Sanderson <katherina.sanderson@gmail.com>
2019-12-07 15:16:30 -0600

    Day 7, Part 2 solution

---
+++ src/day7/Amplifier.java
@@ -0,0 +1,165 @@
+package day7;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+class Amplifier {
+    // program inputs
+    private final List<Integer> list;
+    private final Queue<Integer> inputNumbers;
+    private Amplifier nextAmplifier;
+
+    // instruction
+    private int current;
+    private int opcode;
+    private int[] parameterModes;
+    private int[] parameters;
+
+    // program outputs
+    private int output;
+    private int instructionPointer;
+    private boolean hasInstructionPointerBeenModified;
+
+    Amplifier(List<Integer> list) {
+        this.list = list;
+        inputNumbers = new LinkedList<>();
+    }
+
+    void processInstruction(int current) throws InterruptedException {
+        this.current = current;
+        // opcode is stored in right two digits of instruction's first value
+        opcode = list.get(current) % 100;
+
+        if (opcode == 3 || opcode == 4) {
+            parameterModes = new int[1];
+            parameters = new int[1];
+        } else if (opcode == 5 || opcode == 6) {
+            parameterModes = new int[2];
+            parameters = new int[2];
+        } else if (opcode == 1 || opcode == 2 || opcode == 7 || opcode == 8) {
+            parameterModes = new int[3];
+            parameters = new int[3];
+        } else if (opcode == 99) {
+            // opcode 99 should never be executed; is included to show is valid opcode
+            parameterModes = new int[0];
+            parameters = new int[0];
+        } else {
+            throw new IllegalArgumentException("Unknown opcode [" + opcode + "]");
+        }
+
+        parseParameterModes();
+        parseParameters();
+
+        switch (opcode) {
+            case 1 :
+                int firstParameter = getParameterByMode(0);
+                int secondParameter = getParameterByMode(1);
+                int nextValue = firstParameter + secondParameter;
+                list.set(parameters[2], nextValue);
+                break;
+            case 2 :
+                firstParameter = getParameterByMode(0);
+                secondParameter = getParameterByMode(1);
+                nextValue = firstParameter * secondParameter;
+                list.set(parameters[2], nextValue);
+                break;
+            case 3 :
+                // wait for added inputs if no inputs for this Amplifier
+                synchronized (this) {
+                    while (inputNumbers.isEmpty()) {
+                        wait();
+                    }
+                }
+                list.set(parameters[0], inputNumbers.poll());
+                break;
+            case 4 :
+                output = getParameterByMode(0);
+                synchronized (nextAmplifier) {
+                    nextAmplifier.addInputNumber(output);
+                    if (nextAmplifier.inputNumbers.size() == 1) {
+                        nextAmplifier.notify();
+                    }
+                }
+                break;
+            case 5 :
+                if (getParameterByMode(0) != 0) {
+                    instructionPointer = getParameterByMode(1);
+                    hasInstructionPointerBeenModified = true;
+                }
+                break;
+            case 6 :
+                if (getParameterByMode(0) == 0) {
+                    instructionPointer = getParameterByMode(1);
+                    hasInstructionPointerBeenModified = true;
+                }
+                break;
+            case 7 :
+                int val = getParameterByMode(0) < getParameterByMode(1) ? 1 : 0;
+                list.set(parameters[2], val);
+                break;
+            case 8 :
+                val = getParameterByMode(0) == getParameterByMode(1) ? 1 : 0;
+                list.set(parameters[2], val);
+                break;
+            case 99 :
+                // does nothing
+                break;
+            default :
+                throw new IllegalArgumentException("Unknown opcode [" + opcode + "]");
+        }
+    }
+
+    private int getParameterByMode(int i) {
+        // mode == 0 -> position mode
+        // mode == 1 -> immediate mode
+        return parameterModes[i] == 0 ? list.get(parameters[i]) : parameters[i];
+    }
+
+    private void parseParameterModes() {
+        // remove last 2 digits - they contain the opcode
+        int modes = list.get(current) / 100;
+        for (int i = 0; i < parameterModes.length; i++) {
+            parameterModes[i] = modes % 10;
+            modes /= 10;
+        }
+    }
+
+    private void parseParameters() {
+        for (int i = 0; i < parameters.length; i++) {
+            parameters[i] = list.get(current + 1 + i);
+        }
+    }
+
+    void addInputNumber(int input) {
+        inputNumbers.offer(input);
+    }
+
+    boolean hasInstructionPointerBeenModified() {
+        return hasInstructionPointerBeenModified;
+    }
+
+    void resetInstructionPointer() {
+        hasInstructionPointerBeenModified = false;
+    }
+
+    int getInstructionPointer() {
+        return instructionPointer;
+    }
+
+    int getNumValues() {
+        return 1 + parameters.length;
+    }
+
+    List<Integer> getList() {
+        return list;
+    }
+
+    int getOutput() {
+        return output;
+    }
+
+    void setNextAmplifier(Amplifier amplifier) {
+        nextAmplifier = amplifier;
+    }
+}
--- src/day7/Day7.java
+++ src/day7/Day7.java
@@ -10,58 +10,72 @@ import java.util.stream.Collectors;
 
 public class Day7 {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, InterruptedException {
         List<Integer> input = Files.readAllLines(FileSystems.getDefault().getPath("src", "day7", "input.txt")).stream()
                 .flatMap(str -> Arrays.stream(str.split(",")))
                 .map(Integer::parseInt).collect(Collectors.toList());
 
-        List<List<Integer>> phaseSequences = findValidPhaseSequence();
+        List<List<Integer>> phaseSequencesP1 = findValidPhaseSequence(0, 5);
+        List<List<Integer>> phaseSequencesP2 = findValidPhaseSequence(5, 10);
 
-        int maxOutput = Integer.MIN_VALUE;
-        for (List<Integer> phaseSequence : phaseSequences) {
-            int inputNumber = 0;
-            for (int phase : phaseSequence) {
-                List<Integer> copyList = new ArrayList<>(input);
-                Instruction.setInputNumbers(phase, inputNumber);
-                int current = 0;
-                while (!copyList.get(current).equals(99)) {
-                    Instruction instruction = Instruction.createInstruction(copyList, current);
-                    if (instruction.getOpcode() == 4) {
-                        inputNumber = instruction.getOutput();
-                    }
-                    if (instruction.getHasInstuctionPointerBeenModified()) {
-                        current = instruction.getInstructionPointer();
-                    } else {
-                        current += instruction.getNumValues();
-                    }
-                }
-            }
-            maxOutput = Math.max(maxOutput, inputNumber);
-        }
-        System.out.println("Part 1: " + maxOutput);
+        System.out.println("Part 1: " + findMaxOutputFromPhases(input, phaseSequencesP1));
+        System.out.println("Part 2: " + findMaxOutputFromPhases(input, phaseSequencesP2));
     }
 
-    private static List<List<Integer>> findValidPhaseSequence() {
+    private static int findMaxOutputFromPhases(List<Integer> input, List<List<Integer>> phaseSequences) throws InterruptedException {
+        int maxOutput = Integer.MIN_VALUE;
+        for (List<Integer> phaseSequence : phaseSequences) {
+            // initialize amplifiers with initial input (phase)
+            List<Amplifier> amplifiers = new ArrayList<>();
+            for (int phase : phaseSequence) {
+                Amplifier amplifier = new Amplifier(new ArrayList<>(input));
+                amplifier.addInputNumber(phase);
+                amplifiers.add(amplifier);
+            }
+            amplifiers.get(0).addInputNumber(0);
+
+            // connect amplifiers (in circle: A -> B, B -> C, ..., E -> A)
+            for (int i = 0; i < amplifiers.size() - 1; i++) {
+                amplifiers.get(i).setNextAmplifier(amplifiers.get(i + 1));
+            }
+            amplifiers.get(amplifiers.size() - 1).setNextAmplifier(amplifiers.get(0));
+
+            // start threads
+            List<Thread> threadPool = new ArrayList<>();
+            for (Amplifier amplifier : amplifiers) {
+                Thread thread = new Thread(new AmplifierRunnable(amplifier));
+                thread.start();
+                threadPool.add(thread);
+            }
+
+            // join on the last thread, which signals programs are finished
+            threadPool.get(amplifiers.size() - 1).join();
+            maxOutput = Math.max(maxOutput, amplifiers.get(amplifiers.size() - 1).getOutput());
+        }
+        return maxOutput;
+    }
+
+    private static List<List<Integer>> findValidPhaseSequence(int low, int high) {
         List<List<Integer>> phaseSequences = new ArrayList<>();
-        for (int i = 0; i < 5; i++) {
+        for (int i = low; i < high; i++) {
             List<Integer> phaseSequence = new ArrayList<>();
             phaseSequence.add(i);
-            for (int j = 0; j < 5; j++) {
+            for (int j = low; j < high; j++) {
                 List<Integer> phaseSequence1 = new ArrayList<>(phaseSequence);
                 if (!phaseSequence1.contains(j)) {
                     phaseSequence1.add(j);
                 }
-                for (int k = 0; k < 5; k++) {
+                for (int k = low; k < high; k++) {
                     List<Integer> phaseSequence2 = new ArrayList<>(phaseSequence1);
                     if (!phaseSequence2.contains(k)) {
                         phaseSequence2.add(k);
                     }
-                    for (int l = 0; l < 5; l++) {
+                    for (int l = low; l < high; l++) {
                         List<Integer> phaseSequence3 = new ArrayList<>(phaseSequence2);
                         if (!phaseSequence3.contains(l)) {
                             phaseSequence3.add(l);
                         }
-                        for (int m = 0; m < 5; m++) {
+                        for (int m = low; m < high; m++) {
                             if (!phaseSequence3.contains(m)) {
                                 phaseSequence3.add(m);
                                 if (phaseSequence3.size() == 5) {
